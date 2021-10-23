ce545c333e8ea5779cf6c731a734e74cd43db5e6
ryuichi <rusami.usual@icloud.com>
2021-10-23 13:32:58 +0900

    commit to the 2021/october

--- sample.rb
+++ sample.rb
@@ -1,2 +1,9 @@
-puts "Hello, World!"
-puts 1 + 2
+(2..100).each do |candidate|
+    sqrt = Math.sqrt(candidate)
+    factor_found = (2..sqrt).any? {|i| candidate % i ==0}
+    if factor_found then
+        print "#{candidate}は合成数\n"
+    else
+        print "#{candidate}は素数\n"
+    end
+end
