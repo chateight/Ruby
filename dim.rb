78dd22fb018e2290059c319bc9870ba3c18d4ccb
ryuichi <rusami.usual@icloud.com>
2022-02-04 21:10:18 +0900

    # hash was added

--- dim.rb
+++ dim.rb
@@ -4,3 +4,22 @@
 acids = ["adenin", "thymine", "guanine", "cytosine"]
 signs = acids.map{|array| array[0,1]}           # map{&:some_method} : this(&:) calls some_method
 p signs
+#{}
+# hash
+#{}
+book_to_author = {
+    "python" => "flanagan",
+    "programing ruby" => "matz"
+}
+p book_to_author["programing ruby"]
+book_to_author["python"] = "rossum"         # to modify the hash
+book_to_author["the ruby way"] = "fulton"   # to add a hash element
+p book_to_author
+p book_to_author["ruby in nutshell"]        # no matching record returns "nil"
+book_to_author.delete("python")             # to delete a hash element
+p book_to_author
+book_to_author.each do |book, author|       # each records are accessed by each method
+    p "#{book} by #{author}"
+end
+p book_to_author.map{|book, author| "#{book} by #{author}"     # similar to each method
+}
