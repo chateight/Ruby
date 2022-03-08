e4ce7c427f266a8a2a1b8b0e188701764775ed6d
ryuichi <rusami.usual@icloud.com>
2022-03-08 19:58:00 +0900

    # basic file r/w

--- in_out.rb
+++ in_out.rb
@@ -1,5 +1,14 @@
 exp = ARGV.join(' + ')
 sum = 0
 ARGV.each {|ar| sum = sum + ar.to_i }
-#sum = ARGV.inject(0){|subtotal, arg| subtotal + arg.to_i}      #shorter description using inject()
+# sum = ARGV.inject(0){|subtotal, arg| subtotal + arg.to_i}      #shorter description using inject()
 puts "#{exp} = #{sum}"
+#
+#
+File.open("text.txt", 'a'){|f|
+    f.puts Time.now
+}
+
+File.open("text.txt", 'r'){|f|
+    puts f.read
+}
