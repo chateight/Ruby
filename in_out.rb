759c44261bf558e1e369fc85fece588bb96eb0a3
ryuichi <rusami.usual@icloud.com>
2022-03-08 19:58:34 +0900

    # to add comment

--- in_out.rb
+++ in_out.rb
@@ -4,7 +4,7 @@ ARGV.each {|ar| sum = sum + ar.to_i }
 # sum = ARGV.inject(0){|subtotal, arg| subtotal + arg.to_i}      #shorter description using inject()
 puts "#{exp} = #{sum}"
 #
-#
+# file r/w
 File.open("text.txt", 'a'){|f|
     f.puts Time.now
 }
